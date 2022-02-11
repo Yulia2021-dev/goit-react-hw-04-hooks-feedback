7c4b768229e4df4026749478cf9592831b4b2f39
Yulia <ulaboyko@ukr.net>
2022-02-11 21:24:08 +0200

    Do some refactoring

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,42 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Section from '../section/Section';
-import Notification from '../notification/Notification';
+import React from "react";
+import PropTypes from "prop-types";
+import Section from "../section/Section";
+import Notification from "../notification/Notification";
 
-import s from './statistics.css';
+import s from "./statistics.css";
 
-function Statistics({ good, neutral, bad, total, positivePercentage }) {
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <Section title="Statistics">
-      {!total
-        ? <Notification message="No feedback given" />
-        : (
-          <div className={s.Statistics}>
+      {!total ? (
+        <Notification message="No feedback given" />
+      ) : (
+        <div className={s.Statistics}>
           <ul className="param-list">
-            <li className="Statistics__item">
-              Good: {good}
-            </li>
-            <li className="Statistics__item">
-              Neutral: {neutral}
-            </li>
-            <li className="Statistics__item">
-              Bad: {bad}
-            </li>
-            <li className="Statistics__item">
-              Total: {total}
-            </li>
+            <li className="Statistics__item">Good: {good}</li>
+            <li className="Statistics__item">Neutral: {neutral}</li>
+            <li className="Statistics__item">Bad: {bad}</li>
+            <li className="Statistics__item">Total: {total}</li>
             <li className="Statistics__item">
               Positive: {positivePercentage}%
             </li>
           </ul>
         </div>
-      )
-      }
+      )}
     </Section>
   );
-}
-
-
+};
 
 Statistics.propTypes = {
   good: PropTypes.number,
@@ -45,6 +34,5 @@ Statistics.propTypes = {
   total: PropTypes.number,
   positivePercentage: PropTypes.number,
 };
-
 
 export default Statistics;
